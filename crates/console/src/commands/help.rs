013ee0203baaf90c5989991f133997b0b06ae992
DudeGunter <guntertheguy@gmail.com>
2026-01-04 16:36:57 -0800

    very simple command

--- crates/console/src/commands/help.rs
+++ crates/console/src/commands/help.rs
@@ -2,9 +2,7 @@ use crate::prelude::*;
 use bevy::prelude::*;
 
 pub fn help(In(_arguments): In<String>, config: Res<ConsoleConfig>) {
-    let console_commands = config.get_commands();
-
-    for command in console_commands {
+    for command in config.get_commands() {
         simple!("{}", command);
         if let Some(metadata) = config.get_metadata(command) {
             simple!(" >Description: {}", metadata.description);
